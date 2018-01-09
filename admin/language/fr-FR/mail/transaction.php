5c54759272a6ad96cfdcb76d4b4cb69fc3c14e1b
Gilles <gilles.gelan@izile.fr>
2018-01-09 12:22:22 -0400

    Modification mail notifications avoir

--- admin/language/fr-FR/mail/transaction.php
+++ admin/language/fr-FR/mail/transaction.php
@@ -1,6 +1,6 @@
 <?php
 // Text
 $_['text_subject']  = '%s - Affiliate Credit';
-$_['text_received'] = 'Vous avez reçu %s de cr&eacutedit!';
-$_['text_total']    = 'Le montant total de votre crédit est de %s.';
-$_['text_credit']   = 'Le montant de votre crédit pourra être déduit de votre prochaine commande.';
+$_['text_received'] = 'F&eacutelicitations vous avez re&ccedilu %s de cr&eacutedit!';
+$_['text_total']    = 'Le montant total de votre cr&eacutedit est maintenant de %s.';
+$_['text_credit']   = 'Ce montant pourra &ecirctre d&eacuteduit de votre prochaine commande. L&acute&eacutequipe vous remercie pour votre confiance et &egravesp&egravere vous revoir tres bient&ocirct.';
