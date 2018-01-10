0bd569b2c265834161bc8217758f9dc9c2c689d7
shyndev <shyn.developpeur@gmail.com>
2018-01-10 09:03:32 -0400

    Test de commit

--- catalog/controller/checkout/cart.php
+++ catalog/controller/checkout/cart.php
@@ -56,6 +56,7 @@ class ControllerCheckoutCart extends Controller {
 			$data['products'] = array();
 
 			$products = $this->cart->getProducts();
+			var_dump($this->cart->getProducts());
 
 			foreach ($products as $product) {
 				$product_total = 0;
@@ -66,9 +67,9 @@ class ControllerCheckoutCart extends Controller {
 					}
 				}
 
-				if ($product['minimum'] > $product_total) {
+				// if ($product['minimum'] > $product_total) {
 					$data['error_warning'] = sprintf($this->language->get('error_minimum'), $product['name'], $product['minimum']);
-				}
+				// }
 
 				if ($product['image']) {
 					$image = $this->model_tool_image->resize($product['image'], $this->config->get('theme_' . $this->config->get('config_theme') . '_image_cart_width'), $this->config->get('theme_' . $this->config->get('config_theme') . '_image_cart_height'));
@@ -261,6 +262,7 @@ class ControllerCheckoutCart extends Controller {
 	}
 
 	public function add() {
+
 		$this->load->language('checkout/cart');
 
 		$json = array();
